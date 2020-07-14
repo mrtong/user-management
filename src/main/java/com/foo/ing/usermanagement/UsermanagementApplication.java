6737dc49b31d3136b205e550a85492c94dfa1695
mrtong <yanjun.tong@gmail.com>
2020-07-14 10:40:13 +1000

    add CircuitBreaker

--- src/main/java/com/foo/ing/usermanagement/UsermanagementApplication.java
+++ src/main/java/com/foo/ing/usermanagement/UsermanagementApplication.java
@@ -2,8 +2,12 @@ package com.foo.ing.usermanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
 @SpringBootApplication
+@EnableCaching
+@EnableCircuitBreaker
 public class UsermanagementApplication {
 
     public static void main(String[] args) {
