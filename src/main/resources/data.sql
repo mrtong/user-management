2fd44e29e1bf20da455ab874e95eb2d19375ce14
mrtong <yanjun.tong@gmail.com>
2020-07-14 15:59:17 +1000

    add h2 init data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into ADDRESSES(city,postcode,state,street) values('Sydney','2000','NSW','abc street');
+insert into ADDRESSES(city,postcode,state,street) values('Melbourne','1000','VIC','fff street');
+insert into ADDRESSES(city,postcode,state,street) values('Hongkong','3000','HK','Vicv street');
+insert into user_details(first_name,last_name,gender,title,address_id) values('Luke','Tong','Male','Mr',1);
+insert into user_details(first_name,last_name,gender,title,address_id) values('Captain','Marvel','Female','Ms',3);
