e46f9558cd3c648d247dc67814df5e2b5af4a62e
mrtong <yanjun.tong@gmail.com>
2020-07-14 10:12:30 +1000

    add application properties

---
+++ src/test/java/com/foo/ing/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foo.ing.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
