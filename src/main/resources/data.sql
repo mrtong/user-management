c192baf6acafd0b56e0025cb07b0258bf47426bc
mrtong <yanjun.tong@gmail.com>
2020-07-17 14:19:28 +1000

    add more fake data for users

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,12 @@
 insert into ADDRESSES(city,postcode,state,street) values('Sydney','2000','NSW','abc street');
 insert into ADDRESSES(city,postcode,state,street) values('Melbourne','1000','VIC','fff street');
 insert into ADDRESSES(city,postcode,state,street) values('Hongkong','3000','HK','Vicv street');
+
 insert into user_details(first_name,last_name,gender,title,address_id) values('Luke','Tong','Male','Mr',1);
 insert into user_details(first_name,last_name,gender,title,address_id) values('Captain','Marvel','Female','Ms',3);
+insert into user_details(first_name,last_name,gender,title,address_id) values('Man','Iran','Male','Mr',2);
+insert into user_details(first_name,last_name,gender,title,address_id) values('Foo','Doo','Female','Ms',1);
+insert into user_details(first_name,last_name,gender,title,address_id) values('John','Smith','Male','Mr',2);
+insert into user_details(first_name,last_name,gender,title,address_id) values('Don','Trump','Male','Mr',1);
+insert into user_details(first_name,last_name,gender,title,address_id) values('Bill','Clinton','Male','Ms',3);
+insert into user_details(first_name,last_name,gender,title,address_id) values('Will','Smith','Female','Ms',3);
